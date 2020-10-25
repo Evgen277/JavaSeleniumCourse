91fac0f3da207637cfa0865d59ad4a2d2ebc7d4a
Evgen277 <instant277@gmail.com>
2020-10-25 20:02:31 +0300

    Java Selenium Course - добавление файлов Урок5 Упр9, добавил метод проверки сортировки списков, дописал задание 9 А и Б

--- java-exam/src/test/java/ru/example/Lesson5Ex9.java
+++ java-exam/src/test/java/ru/example/Lesson5Ex9.java
@@ -1,19 +1,27 @@
 package ru.example;
 
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.json.JsonOutput;
+import org.w3c.dom.ls.LSOutput;
 
+import java.beans.Transient;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import static org.junit.Assert.assertTrue;
 
-public class Lesson5Ex9 extends ConfProperties{
+//Урок 5 Задание 9
 
+public class Lesson5Ex9 extends ConfProperties {
 
     @Test
-    public void testSortCountries() {
+    public void testSortCountriesA() {
         driver.get("http://localhost/litecard/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -25,31 +33,82 @@ public class Lesson5Ex9 extends ConfProperties{
         }
 
         int boxAppsMenuSize = driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).size();
-
         for (int i = 0; i < boxAppsMenuSize; i++) {
             String menuAtt = driver.findElements(By.cssSelector("ul#box-apps-menu li#app- a ")).get(i).getAttribute("innerText");
-            if  (menuAtt.contains(" Countries")) {
+            if (menuAtt.contains(" Countries")) {
                 driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(i).click();
                 List<WebElement> countries = driver.findElements(By.cssSelector("table.dataTable tr.row td:nth-child(5)"));
                 List<String> countriesList = new ArrayList<>();
-                for (int j = 0; j < countries.size(); j++) {
-                    countriesList.add(countries.get(j).getAttribute("innerText"));
+                for (WebElement j : countries) {
+                    countriesList.add(j.getAttribute("innerText"));
                 }
-                System.out.println(countriesList);
+                List<String> countriesSortedList = new ArrayList<>();
+                for (String s : countriesList) {
+                    countriesSortedList.add(s);
+                }
+                Collections.sort(countriesSortedList);
+                assertTrue(countriesList.equals(countriesSortedList));
+
+            }
+        }
+    }
+
+
+    @Test
+    public void sortGeoZones1() {
+        int geoZone = driver.findElements(By.cssSelector("table.dataTable tr.row td:nth-child(6)")).size();
+        for (int t = 0; t < geoZone; t++) {
+
+            if (!driver.findElements(By.cssSelector("table.dataTable tr.row td:nth-child(6)")).get(t).getText().equals("0")) {
+                driver.findElements(By.cssSelector("table.dataTable tr.row td:nth-child(5) a")).get(t).click();
+                List<WebElement> geoZones = driver.findElements(By.cssSelector("table#table-zones tbody tr td:nth-child(3)"));
+                List<String> geoZonesList = new ArrayList<>();
+                for (WebElement l : geoZones) {
+                    geoZonesList.add(l.getAttribute("innerText"));
+                }
+                geoZonesList.remove(geoZonesList.size() - 1);
+                List<String> geoZonesSortedList = new ArrayList<>();
+                for (String b : geoZonesList) {
+                    geoZonesSortedList.add(b);
+                }
+                Collections.sort(geoZonesSortedList);
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                assertTrue(geoZonesList.equals(geoZonesSortedList));
+                driver.get("http://localhost/litecard/admin/?app=countries&doc=countries");
             }
 
 
         }
     }
 
-
-    @After
-    public void stop() {
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    @Test
+    public void sortGeoZones2() {
+        int boxAppsMenuSize1 = driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).size();
+        for (int y = 0; y < boxAppsMenuSize1; y++) {
+            String menuAtt1 = driver.findElements(By.cssSelector("ul#box-apps-menu li#app- a ")).get(y).getAttribute("innerText");
+            if (menuAtt1.contains(" Geo Zones")) {
+                driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(y).click();
+            }
         }
-        driver.quit();
+        int countriesLis = driver.findElements(By.cssSelector("table.dataTable tr.row td:nth-child(3) a")).size();
+        for (int f = 1; f <= countriesLis; f++) {
+            driver.findElements(By.cssSelector("table.dataTable tr.row td a")).get(f).click();
+            List<WebElement> geoZone = driver.findElements(By.cssSelector("table#table-zones tbody tr:not(header) td:nth-child(3) select option[selected]"));
+            List<String> geoZoneList = new ArrayList<>();
+            for (WebElement g : geoZone) {
+                geoZoneList.add(g.getAttribute("innerText"));
+            }
+            List<String> geoZoneSortedList = new ArrayList<>();
+            for (String e : geoZoneList) {
+                geoZoneSortedList.add(e);
+            }
+            Collections.sort(geoZoneSortedList);
+            assertTrue(geoZoneList.equals(geoZoneSortedList));
+            driver.get("http://localhost/litecard/admin/?app=geo_zones&doc=geo_zones");
+        }
     }
 }
